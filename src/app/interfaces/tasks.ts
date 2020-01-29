1df71d2cbdcb8207c9b0896d2c83187ad61686d6
Ana Flávia <anaflaviasfranco@gmail.com>
2020-01-28 21:40:18 -0300

    iniciado criacao de tarefa

---
+++ src/app/interfaces/tasks.ts
@@ -0,0 +1,7 @@
+export interface Tasks {
+    description: string;
+    priority: string;
+    todo: boolean;
+    createTime: Date;
+    id: string;
+}
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Tasks } from '../interfaces/tasks';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  private DB_PATH_TASKS = 'task';
+  private user: any;
+
+  constructor(
+    private authService: AuthService,
+    private afStore: AngularFirestore
+  ) { 
+    this.authService.user$.subscribe(user => this.user = user);
+  }
+
+  public create(task: Tasks): Promise<any>{
+    if(!this.user) {
+      return ;
+    }
+
+    const userTasks: AngularFirestoreDocument<any> = this.afStore.doc(`${DB_PATH_TASKS}/${this.user.uid}`);
+    return userTasks.collection('tasks').add(task);
+
+  }
+
+  public delete(task: Tasks): Promise<any> {
+
+  }
+
+  public update(task: Tasks): Promise<any> {
+
+  }
+
+  public getAll(): Observable<any> {
+
+
+  }
+
+
+
 }
